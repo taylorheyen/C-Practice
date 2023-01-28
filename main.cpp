de5169911d8d90950fe977949a2c24269430b95a
Taylor Heyen <taylordheyen@gmail.com>
2023-01-28 15:47:58 -0800

    Mosh Sales Tax Practice

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int favorite_number;
-    std::cout << "Enter your favorite number between 1 and 100:";
+    int sales = 95000;
+    double state_tax = sales * .04;
+    double county_tax =  sales * .02;
+    double after_tax = sales - (state_tax + county_tax);
 
-    std::cin >> favorite_number;
-
-    std::cout << "Amazing! That is my favorite number too!" << std::endl;
-
+    cout << "Sales: $" << sales << endl << "State Tax Deduction: $"
+         << state_tax << endl << "County Tax Deduction: $" << county_tax << endl
+         << "After Tax Deductions: $" << after_tax;
     return 0;
 }
